19d106b414f85ec3e7fad95b522b4833879d2f38
lmblossom <lmblossom@163.com>
2022-07-27 13:35:40 +0800

    msg

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,128 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="root">
+    <div class="todo-container">
+      <div class="todo-wrap">
+        <MyHeader @addTodo="addTodo" />
+        <MyList :todos="todos" />
+        <MyFooter :todos="todos" @checkAllTodo="checkAllTodo" @clearAllTodo="clearAllTodo" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import pubsub from 'pubsub-js'
+import MyHeader from './components/MyHeader.vue'
+import MyFooter from './components/MyFooter.vue'
+import MyList from './components/MyList.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      todos: JSON.parse(localStorage.getItem('todos')) || []
+    }
+  },
+  components: { MyHeader, MyList, MyFooter },
+  methods: {
+    addTodo(todoObj) {
+      this.todos.unshift(todoObj)
+    },
+    checkTodo(id) {
+      this.todos.forEach(todo => {
+        if (todo.id === id) todo.done = !todo.done
+      })
+    },
+    deleteTodo(_, id) {
+      this.todos = this.todos.filter(todo => {
+        return todo.id !== id
+      })
+    },
+    checkAllTodo(done) {
+      this.todos.forEach(todo => {
+        todo.done = done
+      })
+    },
+    clearAllTodo() {
+      this.todos = this.todos.filter(todo => {
+        return !todo.done
+      })
+    },
+    updateTodo(id, title) {
+      this.todos.forEach(todo => {
+        if (todo.id === id) todo.title = title
+      })
+    }
+  },
+  watch: {
+    todos: {
+      deep: true,
+      handler(value) {
+        localStorage.setItem('todos', JSON.stringify(value))
+      }
+    }
+  },
+  mounted() {
+    this.$bus.$on('clearTodo', this.checkTodo)
+    this.$bus.$on('updateTodo', this.updateTodo)
+    // this.$bus.$on('deleteTodo', this.deleteTodo)
+    this.pubId = pubsub.subscribe('deleteTodo', this.deleteTodo)
+  },
+  beforeDestroy() {
+    this.$bus.$off('clearTodo')
+    this.$bus.$off('updateTodo')
+    pubsub.unsubscribe(this.pubId)
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+/*base*/
+body {
+  background: #fff;
+}
+
+.btn {
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+.btn-edit {
+  color: #fff;
+  background-color: skyblue;
+  border: 1px solid rgb(85, 128, 145);
+  margin-right: 5px;
+}
+
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+
 new Vue({
   render: h => h(App),
+  beforeCreate() {
+    Vue.prototype.$bus = this
+  },
 }).$mount('#app')
